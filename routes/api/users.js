17f6b31975387c27c8c4878bf3afb7275a36fbef
ShaunGerardAlberts <alberts.shaun@gmail.com>
2018-10-20 10:43:59 +1000

    Validation and profile model added

--- routes/api/users.js
+++ routes/api/users.js
@@ -11,6 +11,7 @@ const User = require('../../models/User')
 
 // Load Input Validation
 const validateRegisterInput = require('../../validation/register')
+const validateLoginInput = require('../../validation/login')
 
 // @router  GET api/users/test
 // @desc    Tests users routes
@@ -63,6 +64,12 @@ router.post('/register', (req, res) => {
 // @desc    Login user / Returning JWT Token
 // @access  Public
 router.post('/login', (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body)
+
+  // Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors)
+  }
   const email = req.body.email
   const password = req.body.password
 
@@ -71,7 +78,8 @@ router.post('/login', (req, res) => {
     .then(user => {
       // Check for user
       if (!user) {
-        return res.status(404).json({ email: 'User not found' })
+        errors.email = 'User not found'
+        return res.status(404).json(errors)
       }
 
       // Check Password
@@ -92,7 +100,8 @@ router.post('/login', (req, res) => {
                 })
               })
           } else {
-            return res.status(400).json({ password: 'Password incorrect' })
+            errors.password = 'Password incorrect'
+            return res.status(400).json(errors)
           }
         })
     })
